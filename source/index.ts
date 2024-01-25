024865432093ad86b5e097438d8bc120768963bf
Nuñez <carlos.nunez.calvario@yandex.com>
2024-01-24 20:36:35 -0600

    Added module compatibility

---
+++ source/index.ts
@@ -0,0 +1,7 @@
+export {Kardex} from './modules/Kardex.js'
+export {PaymentOrder} from './modules/PaymentOrder.js'
+export { Login } from './modules/login.js'
+export {BasicData} from './modules/student.js'
+
+export type * as Student from './modules/student.js';
+export type {Credentials} from './modules/login.js';
